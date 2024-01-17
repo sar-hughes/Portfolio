7181adde4fd7efe9043233243dec45ae6bf2467a
Sarah hughes <133598916+sar-hughes@users.noreply.github.com>
2024-01-17 14:42:09 -0400

    only tested js index.js

--- index.js
+++ index.js
@@ -4,10 +4,3 @@ function copyEmail(){
     navigator.clipboard.writeText(email);
 }
 
-const secondPage = document.getElementById("overlap-2").offsetTop;
-function onScroll(){
-       // window.scrollTo({top: secondPage, behavior: "smooth"});
-       document.querySelector('.overlap-2').scrollIntoView({
-        behavior: "smooth"
-       });
-}
